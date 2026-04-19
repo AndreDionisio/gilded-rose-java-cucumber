b2fc82c8b4909632e26280ed555127805654ecfe
AndreDionisio <andreluisdionisio@gmail.com>
2026-04-19 13:37:44 +0200

    refactor: introduce immutable records for item state and rules

--- src/main/java/com/gildedrose/rules/BetterItem.java
+++ src/main/java/com/gildedrose/rules/BetterItem.java
@@ -1,14 +1,11 @@
 package com.gildedrose.rules;
 
-public record Item(String name, Expiration expiration, Quality quality) {
+public record BetterItem(String name, Expiration expiration, Quality quality) {
 
-
-
-    // Métodos delegados (Encapsulamento)
-    public void updateExpiration() { expiration.update(); }
-    public Quality getQuality() { return quality; }
-    public Expiration getExpiration() { return expiration; }
-    public Item tick() {
-        return new Item(name, expiration.nextDay(), quality);
+    public BetterItem tick() {
+        return new BetterItem(name, expiration.nextDay(), quality);
+    }
+    public BetterItem withQuality(Quality newQuality) {
+        return new BetterItem(name, expiration, newQuality);
     }
 }
