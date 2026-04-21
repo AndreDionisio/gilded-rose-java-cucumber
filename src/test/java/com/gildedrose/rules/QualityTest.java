3097c1826c22068d6db480bce62c5b9378bf5acb
AndreDionisio <andreluisdionisio@gmail.com>
2026-04-21 17:02:19 +0200

    test: refactor mov DomainConstants

--- src/test/java/com/gildedrose/rules/QualityTest.java
+++ src/test/java/com/gildedrose/rules/QualityTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static com.gildedrose.rules.DomainConstants.*;
+import static com.gildedrose.constants.DomainConstants.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class QualityTest {
