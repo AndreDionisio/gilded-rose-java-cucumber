fc03f76e54bc45e866e5d738bba03552a8d2e722
AndreDionisio <andreluisdionisio@gmail.com>
2026-05-01 11:22:24 +0200

    refactor: spring boot test

--- src/test/java/com/gildedrose/CucumberConfig.java
+++ src/test/java/com/gildedrose/CucumberConfig.java
@@ -1,4 +1,9 @@
 package com.gildedrose;
 
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@CucumberContextConfiguration
+@SpringBootTest(classes = GildedRoseApplication.class)
 public class CucumberConfig {
 }
