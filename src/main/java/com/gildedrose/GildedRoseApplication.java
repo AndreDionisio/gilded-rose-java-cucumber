8b7dc008123e8fa8a80b9ea2926fa797f2e5967f
AndreDionisio <andreluisdionisio@gmail.com>
2026-05-01 11:39:05 +0200

    refactor: spring boot add application

--- src/main/java/com/gildedrose/GildedRoseApplication.java
+++ src/main/java/com/gildedrose/GildedRoseApplication.java
@@ -1,4 +1,18 @@
 package com.gildedrose;
 
-public class GildedRoseApplication {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GildedRoseApplication implements CommandLineRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(GildedRoseApplication.class, args);
+    }
+    @Override
+    public void run(String... args) {
+        System.out.println("--- Gilded Rose Log Iniciado ---");
+
+        System.out.println("--- Processamento Finalizado ---");
+    }
 }
